36e6ce0a06e83c32a0ffa5991f0ba685757712a2
KhanPodMiu <miupod.3006@gmail.com>
2025-07-17 11:21:02 +0700

    Initial Commit

---
+++ Folder_data/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Folder_data
+//
+//  Created by PodMiu on 17/7/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
